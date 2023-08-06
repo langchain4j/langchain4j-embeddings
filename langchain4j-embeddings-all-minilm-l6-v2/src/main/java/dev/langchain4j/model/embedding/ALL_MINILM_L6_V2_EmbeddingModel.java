360afe0507aae5d7212a92249e5e3c13f28f7068
Shastick <julien.perro@gmail.com>
2023-08-06 18:53:58 +0200

    Migrate embeddings from main repo

---
+++ langchain4j-embeddings-all-minilm-l6-v2/src/main/java/dev/langchain4j/model/embedding/ALL_MINILM_L6_V2_EmbeddingModel.java
@@ -0,0 +1,25 @@
+package dev.langchain4j.model.embedding;
+
+import dev.langchain4j.data.segment.TextSegment;
+
+import java.util.List;
+
+public class ALL_MINILM_L6_V2_EmbeddingModel extends AbstractInProcessEmbeddingModel {
+
+    private static final OnnxBertEmbeddingModel MODEL = load("/all-minilm-l6-v2.onnx");
+
+    @Override
+    protected OnnxBertEmbeddingModel model() {
+        return MODEL;
+    }
+
+    @Override
+    // TODO remove before merging and once 0.19.0 was published
+    public int estimateTokenCount(List<TextSegment> textSegments) {
+        int tokenCount = 0;
+        for (TextSegment textSegment : textSegments) {
+            tokenCount += estimateTokenCount(textSegment);
+        }
+        return tokenCount;
+    }
+}
