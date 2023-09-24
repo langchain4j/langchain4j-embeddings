b0b06b62ef5be96485a2b59a5942acfcb5c36d8e
deep-learning-dynamo <deep-learning-dynamo@gmail.com>
2023-09-24 10:49:35 +0200

    Renamed CustomEmbeddingModel into OnnxEmbeddingModel

--- langchain4j-embeddings/src/main/java/dev/langchain4j/model/embedding/OnnxEmbeddingModel.java
+++ langchain4j-embeddings/src/main/java/dev/langchain4j/model/embedding/OnnxEmbeddingModel.java
@@ -4,30 +4,26 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 
 /**
- * A custom embedding model that runs within your Java application's process.
+ * An embedding model that runs within your Java application's process.
  * Any BERT-based model (e.g., from HuggingFace) can be used, as long as it is in ONNX format.
  * Information on how to convert models into ONNX format can be found <a href="https://huggingface.co/docs/optimum/exporters/onnx/usage_guides/export_a_model">here</a>.
  * Many models already converted to ONNX format are available <a href="https://huggingface.co/Xenova">here</a>.
  */
-public class CustomEmbeddingModel extends AbstractInProcessEmbeddingModel {
+public class OnnxEmbeddingModel extends AbstractInProcessEmbeddingModel {
 
     private final OnnxBertBiEncoder model;
 
     /**
-     * Loads a custom embedding model.
-     *
      * @param pathToModel The path to the .onnx model file (e.g., "/home/me/model.onnx").
      */
-    public CustomEmbeddingModel(Path pathToModel) {
+    public OnnxEmbeddingModel(Path pathToModel) {
         model = loadFromFileSystem(pathToModel);
     }
 
     /**
-     * Loads a custom embedding model.
-     *
      * @param pathToModel The path to the .onnx model file (e.g., "/home/me/model.onnx").
      */
-    public CustomEmbeddingModel(String pathToModel) {
+    public OnnxEmbeddingModel(String pathToModel) {
         this(Paths.get(pathToModel));
     }
 
