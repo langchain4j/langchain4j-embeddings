45a97b61c369b40715547df73a9bdcd7bb9f227b
jiangsier-xyz <126842484+jiangsier-xyz@users.noreply.github.com>
2024-05-23 14:56:33 +0800

    Fix copy-paste errors.

--- langchain4j-embeddings-bge-small-zh-v15-q/src/main/java/dev/langchain4j/model/embedding/bge/small/zh/v15/BgeSmallZhV15QuantizedEmbeddingModel.java
+++ langchain4j-embeddings-bge-small-zh-v15-q/src/main/java/dev/langchain4j/model/embedding/bge/small/zh/v15/BgeSmallZhV15QuantizedEmbeddingModel.java
@@ -5,17 +5,17 @@ import dev.langchain4j.model.embedding.OnnxBertBiEncoder;
 import dev.langchain4j.model.embedding.PoolingMode;
 
 /**
- * Quantized BAAI bge-small-en-v1.5 embedding model that runs within your Java application's process.
+ * Quantized BAAI bge-small-zh-v1.5 embedding model that runs within your Java application's process.
  * <p>
  * Maximum length of text (in tokens) that can be embedded at once: unlimited.
  * However, while you can embed very long texts, the quality of the embedding degrades as the text lengthens.
  * It is recommended to embed segments of no more than 512 tokens long.
  * <p>
- * Embedding dimensions: 384
+ * Embedding dimensions: 512
  * <p>
- * It is recommended to add "Represent this sentence for searching relevant passages:" prefix to a query.
+ * It is recommended to add "为这个句子生成表示以用于检索相关文章：" prefix to a query.
  * <p>
- * More details <a href="https://huggingface.co/BAAI/bge-small-en-v1.5">here</a>
+ * More details <a href="https://huggingface.co/BAAI/bge-small-zh-v1.5">here</a>
  */
 public class BgeSmallZhV15QuantizedEmbeddingModel extends AbstractInProcessEmbeddingModel {
 
